468ccd73ea93624de7e743b47b5798f2bf318eb3
Jeff Terry <terryj@iit.edu>
2021-05-07 10:42:18 -0500

    Initial Commit

---
+++ Shared/Metropolis_With_PlotApp.swift
@@ -0,0 +1,17 @@
+//
+//  Metropolis_With_PlotApp.swift
+//  Shared
+//
+//  Created by Jeff Terry on 5/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Metropolis_With_PlotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
